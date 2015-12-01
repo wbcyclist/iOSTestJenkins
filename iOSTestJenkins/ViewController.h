51453bd5576eec393d14bbd2a53b15a346834b28
JasioWoo <wbcyclist@gmail.com>
2015-12-01 11:20:29 +0800

    init

---
+++ iOSTestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSTestJenkins
+//
+//  Created by JasioWoo on 15/12/1.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSTestJenkins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSTestJenkins
+//
+//  Created by JasioWoo on 15/12/1.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
