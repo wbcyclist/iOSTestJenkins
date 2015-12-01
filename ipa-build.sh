f9e7f6f0cecaebf61f4d43fad77a50200b19d797
JasioWoo <wbcyclist@gmail.com>
2015-12-01 19:16:25 +0800

    update

--- ipa-build.sh
+++ ipa-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd ${WORKSPACE}
 pod install --no-repo-update
-xcodebuild -list -workspace iOSTestJenkins.xcworkspace
+#xcodebuild -list -workspace iOSTestJenkins.xcworkspace
 xcodebuild -archivePath "build/test.xcarchive" -workspace iOSTestJenkins.xcworkspace -sdk iphoneos -scheme "iOSTestJenkins" -configuration "AdHoc" archive
 xcodebuild -exportArchive -exportFormat IPA -exportProvisioningProfile "WildcardAppProfile" -archivePath "build/test.xcarchive" -exportPath "build/test_AdHoc.ipa"
 echo "Archive Successfully"
